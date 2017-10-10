fed6e95a20084ab5538e12de868c6b0b05f9f0bd
Péter Biró <biro.peter.hu@gmail.com>
2017-10-10 11:32:51 +0200

    Level 0 functions for Player class
    moveTo()
    changeHealth()

--- Player.h
+++ Player.h
@@ -5,11 +5,14 @@
 #include "Zone.h"
 #include "Item.h"
 #include "Enemy.h"
-#include "vector"
-#include "map"
+#include <vector>
+#include <map>
 
 class Player {
 public:
+    Player(Zone *pPosition);
+    ~Player();
+
     void attack(Enemy enemy);
     void moveTo(Zone* destination);
     void equip(Item* itemToEquip );
@@ -18,12 +21,18 @@ public:
     void changeHealth(short &damage);
     void use(Item itemToUse);
     void pickup(Item itemToPickup);
+
+    virtual ~Player();
+
 private:
+    const int MAX_HEALTH = 100;
+    const double INV_WEIGHT_LIMIT = 50.00;
+
     std::vector<Item> inventory;
     std::map<enum type, Item> equipped;
     Zone* pPosition;
     short health;
-    double invWeightLimit;
+    double invWeight;
 };
 
 
